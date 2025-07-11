4d1d425ff70ab5f94f4cd20d3f27b629036783a2
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2025-07-11 06:46:35 +0545

    init:project setup

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+application_name = "fibvalues"
+instance_type    = "t2.micro"
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "application_name" {
+  description = "The name of the Elastic Beanstalk application"
+  type        = string
+  default     = "fibvalues"
+}
+
+variable "environment_name" {
+  description = "The name of the Elastic Beanstalk environment"
+  type        = string
+  default     = "fibvalues-env"
+}
+
+variable "solution_stack_name" {
+  description = "The solution stack name for the Elastic Beanstalk environment"
+  type        = string
+  default     = "64bit Amazon Linux 2023 v4.6.0 running Docker"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The type of EC2 instance to use for the Elastic Beanstalk environment"
+}
