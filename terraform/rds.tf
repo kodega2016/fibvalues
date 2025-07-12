6ff0eae70d195070cf1cf2c1d3cb38c60680de96
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2025-07-12 09:43:36 +0545

    fix:workflow

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,15 +1,22 @@
 resource "aws_db_instance" "this" {
+  identifier             = "fibvalues-postgres-db"
   allocated_storage      = 10
   db_name                = var.postgres_db_name
-  engine                 = "postgresql"
-  engine_version         = "8.0"
+  engine                 = "postgres"
+  engine_version         = "15"
   instance_class         = "db.t3.micro"
   username               = var.postgres_username
   password               = var.postgres_password
-  parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.fibvalues_sg_internal.id]
   tags = {
     Name = "fibvalues-postgres-db"
+  }
+
+
+  lifecycle {
+    ignore_changes = [
+      password, # Ignore changes to the password
+    ]
   }
 }
--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,12 +1,11 @@
 resource "aws_elasticache_cluster" "this" {
-  cluster_id           = "fibvalues-redis-cluster"
-  engine               = "redis"
-  node_type            = "t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
-  port                 = 6379
-  security_group_ids   = [aws_security_group.fibvalues_sg_internal.id]
+  cluster_id         = "fibvalues-redis-cluster"
+  engine             = "redis"
+  node_type          = "cache.t2.micro"
+  num_cache_nodes    = 1
+  engine_version     = "7.0"
+  port               = 6379
+  security_group_ids = [aws_security_group.fibvalues_sg_internal.id]
   tags = {
     Name = "fibvalues-redis-cluster"
   }
